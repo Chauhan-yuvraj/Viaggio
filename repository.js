c2155b9cadebe477579ced23bc30a0308d11feb7
CHAUHAN YUVRAJ SINGH <113570060+Chauhan-yuvraj@users.noreply.github.com>
2024-07-08 17:47:04 +0530

    Create repository.js

---
+++ repository.js
@@ -0,0 +1,9 @@
+{
+  "name": "Viaggio : an ecommerce website !",
+  "description": "Welcome to Viaggio India! Explore premium Indian fashion, home decor, and lifestyle products. With our sleek React-powered website and Tailwind CSS styling, shopping is effortless and delightful. Elevate your online shopping experience today!",
+  "year": "2024",
+  "role": "Front-end developer",
+  "avatar": "viaggio.png",
+  "demo": "https://viaggio2.surge.sh/",
+  "github": "https://github.com/Chauhan-yuvraj/Viaggio"
+}
